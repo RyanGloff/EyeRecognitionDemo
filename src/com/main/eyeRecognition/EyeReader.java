4b70b81ad24ecd1b6d952f22519455d703caa3d5
Ryan <ryan.gloff@yahoo.com>
2017-09-13 15:22:15 -0400

    Added the EyeReader and testing for it. Needs testing image data to be tested.

--- src/com/main/eyeRecognition/EyeReader.java
+++ src/com/main/eyeRecognition/EyeReader.java
@@ -1,5 +1,69 @@
 package com.main.eyeRecognition;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
 public class EyeReader {
 
+	private int tolerance = 20;
+	private double threshold = 0;
+	
+	private ArrayList<BufferedImage> openImgs;
+	private ArrayList<BufferedImage> closedImgs;
+	
+	public EyeReader () {
+		openImgs = new ArrayList<BufferedImage>();
+		closedImgs = new ArrayList<BufferedImage>();
+	}
+	
+	public void addOpen (BufferedImage img) {
+		openImgs.add(img);
+	}
+	public void addClosed (BufferedImage img) {
+		closedImgs.add(img);
+	}
+	
+	public void calc () {
+		if (openImgs.size() < 5) throw new IllegalStateException("Not enough training data. Add more openEye images.");
+		if (closedImgs.size() < 5) throw new IllegalStateException("Not enough training data. Add more closedEye images.");
+		
+		int openAvg = 0;
+		for (BufferedImage img : openImgs) {
+			openAvg += getWhiteness(img);
+		}
+		openAvg /= openImgs.size();
+		
+		int closedAvg = 0;
+		for (BufferedImage img : closedImgs) {
+			closedAvg += getWhiteness(img);
+		}
+		closedAvg /= closedImgs.size();
+		threshold = (openAvg + closedAvg) / 2;
+	}
+	
+	public boolean isOpen (BufferedImage img) {
+		if (getWhiteness(img) > threshold) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	private int getWhiteness (BufferedImage img) {
+		int sum = 0;
+		for (int y = 0; y < img.getHeight(); y++) {
+			for (int x = 0; x < img.getWidth(); x++) {
+				Color c = new Color(img.getRGB(x, y));
+				int r = c.getRed();
+				int g = c.getGreen();
+				int b = c.getBlue();
+				if (Math.abs(r - b) + Math.abs(r - g) + Math.abs(b - g) > tolerance) {
+					sum++;
+				}
+			}
+		}
+		return sum;
+	}
+	
 }
---
+++ test/com/main/eyeRecognition/EyeReaderTesting.java
@@ -0,0 +1,73 @@
+package com.main.eyeRecognition;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+
+import org.junit.Test;
+
+public class EyeReaderTesting {
+
+	@Test
+	public void eyeReaderTesting () {
+		// Creating the EyeReader, trainSet and testSet
+		EyeReader er = new EyeReader();
+		File testDir = new File("res/testEyes/");
+		ArrayList<BufferedImage> openTestImgs = new ArrayList<BufferedImage>();
+		ArrayList<BufferedImage> closedTestImgs = new ArrayList<BufferedImage>();
+		for (File file : testDir.listFiles()) {
+			BufferedImage testImg = null;
+			try {
+				testImg = ImageIO.read(file);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if (testImg == null) throw new IllegalStateException("The image was not found");
+			switch (file.getName().charAt(0)) {
+			case 'O':
+				er.addOpen(testImg);
+				break;
+			case 'C':
+				er.addClosed(testImg);
+				break;
+			case 'T':
+				if (file.getName().charAt(1) == 'O') {
+					openTestImgs.add(testImg);
+				} else {
+					closedTestImgs.add(testImg);
+				}
+			}
+		}
+		er.calc();
+		
+		// Testing the test set
+		int succ = 0;
+		int fail = 0;
+		for (BufferedImage img : openTestImgs) {
+			if (er.isOpen(img)) {
+				System.out.println("Success");
+				succ++;
+			} else {
+				System.out.println("Fail");
+				fail++;
+			}
+		}
+		for (BufferedImage img : closedTestImgs) {
+			if (er.isOpen(img)) {
+				System.out.println("Fail");
+				fail++;
+			} else {
+				System.out.println("Success");
+				succ++;
+			}
+		}
+		System.out.println("Succeeded: " + succ);
+		System.out.println("Failed: " + fail);
+		System.out.println("Success Rate: " + (fail != 0 ? ((double)succ / fail) : 1d));
+		
+	}
+	
+}
